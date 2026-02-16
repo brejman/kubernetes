1b2b05c745be2985cddbe2300ec9fb052188a9e2
Bartosz <brejman@google.com>
2026-02-16 16:35:40 +0000

    Add placement generator plugin boilerplate

--- pkg/scheduler/framework/runtime/framework.go
+++ pkg/scheduler/framework/runtime/framework.go
@@ -75,6 +75,8 @@ type frameworkImpl struct {
 	permitPlugins        []fwk.PermitPlugin
 	batchablePlugins     []fwk.SignPlugin
 
+	placementGeneratePlugins []fwk.PlacementGeneratorPlugin
+
 	// pluginsMap contains all plugins, by name.
 	pluginsMap map[string]fwk.Plugin
 
@@ -130,6 +132,7 @@ func (f *frameworkImpl) getExtensionPoints(plugins *config.Plugins) []extensionP
 		{&plugins.Permit, &f.permitPlugins},
 		{&plugins.PreEnqueue, &f.preEnqueuePlugins},
 		{&plugins.QueueSort, &f.queueSortPlugins},
+		{&plugins.PlacementGenerate, &f.placementGeneratePlugins},
 	}
 }
 
